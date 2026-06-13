309df2a28ae883ad9c8b1facab2f7069c46a13e6
dashon1 <dashon1@github.com>
2026-06-13 17:25:08 -0400

    fix: add entity/function/integration stub files

---
+++ src/entities/all.js
@@ -0,0 +1,5 @@
+import { base44 } from '../api/base44Client';
+export const PracticeSession = base44.entities.PracticeSession;
+export const Repertoire = base44.entities.Repertoire;
+export const User = base44.entities.User;
+export const PracticeRoutine = base44.entities.PracticeRoutine;
